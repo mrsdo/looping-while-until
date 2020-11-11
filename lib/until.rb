eab30ead7efdc565d212d7e15040a1ec17a8e480
Martinique <me@martidolce.com>
2020-11-11 14:57:55 -0500

    Updated working copies in Lib directory

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,20 @@
 def using_until
-  levitation_force = 6
   #your code here
-    
+  # counter = 0
+  # until counter == 20
+  # 	puts "The current number is less than 20."
+  # 	counter += 1
+  # end
+
+  levitation_force = 6
+  #text = "Wingardium Leviosa"
+  until  levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1 #
+  end
 end
+using_until
+
+
+
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,30 @@
-def using_while
-  levitation_force = 6
-	
-  #your code here
-end
+# frozen_string_literal: true
 
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+# def using_while
+#   levitation_force = 6
+#
+#   #your code here
+# end
+# num_of_hotdogs_eaten = 0
+# # => 0 (return value)
+#
+# while num_of_hotdogs_eaten < 7
+#   num_of_hotdogs_eaten += 1
+#   puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+# end
+
+# levitation_force = 6 #current
+def using_while
+  levitation_force = 6 #Need
+  #levitation_force = levitation_force + 4 #=>10
+
+  while  levitation_force < 10 #while 0 < 6
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
+end
+using_while
 

f3e8bbab31f4eb8e37de7a977de3027f5ec429be
Martinique <me@martidolce.com>
2020-11-11 14:52:43 -0500

    commit tree 3295d49f8d76c174927d6a9c11e4cede6c335e7a

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,17 @@
-def using_until
+def using_until # Fill out the content of the using_until method
   levitation_force = 6
   #your code here
+
+  
+  #until our levitation force equals 10
+  until levitation_force == 10
+
+  #puts the desired phrase, "Wingardium Leviosa"
+
+  puts "Wingardium Leviosa"
+  # increment your levitation force by 1
+  levitation_force += 1
+  end
     
 end
-
+using_until
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,15 @@
 def using_while
-  levitation_force = 6
+  levitation_force = 6 #Fill out the content of the using_while method
 	
   #your code here
-end
 
+  #while your levitation force is less than 10
+  while levitation_force < 10
+    #Puts some desired phrase
+    puts "Wingardium Leviosa"
+    #increment your levitation force by 1
+    levitation_force += 1
+  end
+end
+using_while
 
